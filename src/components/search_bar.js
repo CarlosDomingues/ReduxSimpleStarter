94f47930b21dd6b21cc0d84c087b873fb4481e21
Carlos Felipe Domingues e Oliveira <cWX498295@china.huawei.com>
2018-05-28 01:32:56 -0300

    First working app.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,8 +10,37 @@ const SearchBar = () => {
 // But we need our component to have state
 // So we use a class based component (has introspection)
 class SearchBar extends React.Component {
+
+    // First and only method when an instance is created
+    // We call super(props) to execute React.Component 
+    // constructore  
+    constructor(props) {
+        super(props);
+
+        // We declare a state wich has a single propriety,
+        // Called "term". We only change the state this way
+        // inside the constructor function  
+        this.state = { term: '' };
+    }
+
     render() {
-        return <input />;
+
+        return (
+        <div className="search-bar">
+            <input 
+            value ={this.state.term}
+            onChange={event => this.onInputChange(event.target.value)} />
+        </div>
+        );
+    }
+
+    // Event handler, is called when the input 
+    // content changes
+    // we could see the actual event using
+    // console.log(event)  
+    onInputChange(term) {
+        this.setState({term})
+        this.props.onSearchTermChange(term);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 // Go find the 'react' library and assign it to the
 // variable 'React'
-import React from 'react';
+import _ from 'lodash'
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
-const API_KEY = 'AIzaSyCYNgJJHXUHoYIvKV8xrz5yP7Pnvse2M7s'
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyCYNgJJHXUHoYIvKV8xrz5yP7Pnvse2M7s';
 
 /*
 Creates a new component that produces html
@@ -16,12 +18,43 @@ This is like a factory for creating App functions.
 = () => is the ES6 equivalent of
 = function ()
 */
-const App = () => {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('Huawei Carrier')
+  }
+
+
+videoSearch(term){
+  YTSearch({ key: API_KEY, term}, (videos) => {
+    // This is equivalent to this.setState ({ videos: videos})
+    this.setState({ 
+      videos : videos,
+      selectedVideo: videos[0]
+    });
+  });
+}
+
+  render() {
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+
     return (
-    <div>
-      <SearchBar />
-    </div>
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}/>
+      </div>
     );
+  }
 }
 
 // Put the generated html and insert it on the DOM
@@ -29,4 +62,4 @@ const App = () => {
 // We also need to tell ReactDOM the target DOM
 // where App will be put
 ReactDOM.render(<App />,
-                document.querySelector('.app-container'));
+  document.querySelector('.app-container'));
